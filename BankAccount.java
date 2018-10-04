e86ffcb4ce02a996ae0bc0e8a10a2814264e7504
jjshawshaw <jshaw00@stuy.edu>
2018-10-03 20:24:17 -0400

    Added constructer and getAccountID and getBalance methods

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,23 @@
 public class BankAccount{
-  
+  // fields
+  private double balance;
+  private int accountID;
+  private String password;
+
+  // constructor
+  public BankAccount(int accountID, double balance, String password){
+    this.balance = balance;
+    this.accountID = accountID;
+    this.password = password;
+  }
+
+  // methods
+  public double getBalance(){
+    return balance;
+  }
+  public int getAccountID(){
+    return accountID;
+  }
+
+
 }
--- Driver.java
+++ Driver.java
@@ -1,4 +1,7 @@
 public class Driver{
   public static void main(String[] args) {
+    BankAccount a = new BankAccount(1, 1000, "123");
+    System.out.println("accountID: " + a.getAccountID());
+    System.out.println("balance: " + a.getBalance());
   }
 }
